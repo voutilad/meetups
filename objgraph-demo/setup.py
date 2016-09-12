2d1a40fb8c496a5d37f4b62619aeb0e2e7807392
Dave Voutila <voutilad@gmail.com>
2016-09-12 10:52:07 -0400

    py.test working

---
+++ objgraph-demo/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='objgraph-demo',
+    version='0.1.0',
+    packages=['bookstore'],
+    url='https://github.com/voutilad/meetups',
+    license='MIT',
+    author='Dave Voutila',
+    author_email='voutilad@gmail.com',
+    description='Example of using Objgraph',
+    test_require=['pytest',]
+)
