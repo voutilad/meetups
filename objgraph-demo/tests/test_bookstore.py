10de71a8784eda50f960663d257b4865d10fad72
Dave Voutila <voutilad@gmail.com>
2016-09-12 13:19:19 -0400

    basic tests for leaks

--- objgraph-demo/tests/test_bookstore.py
+++ objgraph-demo/tests/test_bookstore.py
@@ -1,4 +1,8 @@
 #!/bin/env python
+import gc
+import objgraph
+from io import StringIO
+
 from bookstore import Book, Page
 
 
@@ -18,3 +22,39 @@ def test_building_a_book():
     book.append(page)
     assert book.pages == [page]
     assert len(book) == 1
+
+
+def test_garbage_collection():
+    """
+    Build a Book and see if it gets garbage collected
+    :return: None
+    """
+
+    # we'll use this to collect output from objgraph
+    stdout = StringIO()
+
+    # initialize objgraph's tracking, run it 3 times to get through
+    # dynamic loading of things
+    print('')
+    objgraph.show_growth(file=stdout)
+    objgraph.show_growth(file=stdout)
+    objgraph.show_growth(file=stdout)
+
+    book = Book(title='Total Garbage')
+    book.append(Page(text='This is page 1'))
+    book.append(Page(text='This is page 2'))
+
+    # look for growth
+    print('Growth:\n')
+    objgraph.show_growth()
+
+    # let's destroy our book
+    print('\nBurning our book!')
+    del book
+    gc.collect()
+
+    # look for shrinkage
+    print('\nMake sure nothing is left.')
+    books = objgraph.by_type('Book')
+    print('books == ' + str(books))
+    assert books == []
