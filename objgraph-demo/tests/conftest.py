2c95b228ce1a84e100b85e04e46d60d3a686e0e8
Dave Voutila <voutilad@gmail.com>
2016-09-12 21:15:47 -0400

    manual backport of py.test fixture from fix-01 branch.

--- objgraph-demo/tests/conftest.py
+++ objgraph-demo/tests/conftest.py
@@ -1,8 +1,14 @@
 import pytest
 from bookstore import Book, LeakyBook
+from bookstore.storage import _pages
 
 
 @pytest.fixture(scope='session',
                 params=[(Book, 'Book'), (LeakyBook, 'LeakyBook')])
 def book_fixture(request):
     return request.param
+
+
+@pytest.fixture(autouse=True)
+def clear_pages():
+    _pages.clear()
