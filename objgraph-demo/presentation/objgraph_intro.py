b398a0b40f2e9bb8d807f08685c88c0fbb9b6927
Dave Voutila <voutilad@gmail.com>
2016-09-13 10:26:55 -0400

    small format change for display purposes

--- objgraph-demo/presentation/objgraph_intro.py
+++ objgraph-demo/presentation/objgraph_intro.py
@@ -6,9 +6,11 @@ class Monkey(object):
 
 m1, m2 = Monkey(), Monkey()
 
-print('Monkeys: {}'.format(objgraph.by_type('Monkey')))
+print('Monkeys: {}'
+    .format(objgraph.by_type('Monkey')))
 
 del m1
 del m2
 
-print('Monkeys: {}'.format(objgraph.by_type('Monkey')))
+print('Monkeys: {}'
+    .format(objgraph.by_type('Monkey')))
